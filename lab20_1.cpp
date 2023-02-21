c43f71e6257c5f70bc6838cb10785f7fe4d79c43
Nongdada <ronnachai_si@cmu.ac.th>
2023-02-21 16:56:52 +0700

    "done"

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect A, Rect B){
+	double area1 = min((A.x+A.w),(B.x+B.w))-max(A.x,B.x);
+	double area2 =min((A.y+A.h),(B.y+B.h))-B.y;
+	double sum =0;
+	if(area1 <= 0 || area2 <=0){
+		return sum;
+	}else{
+		sum = area1*area2;
+		return sum;
+	}	
 
-
-
+	
+	
+};
+int main(){
+Rect R1 = {1,1,5,5};
+Rect R2 = {7,2,3,3};	
+cout << overlap(R1,R2);	
 }
+
